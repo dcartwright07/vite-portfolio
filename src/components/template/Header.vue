cfbad52c6f5a15d40f9b2bd91e814853946a27dd
Dominic Cartwright <dcartwright07@users.noreply.github.com>
2021-10-28 00:56:28 -0400

    Update menu links styles

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -6,13 +6,22 @@
       </a>
 
       <nav class="collapse flex flex-row space-x-6">
-        <a href="/" class="current text-gray-500 hover:text-red-700">
+        <a
+          href="/"
+          class="current text-gray-500 hover:text-red-700 uppercase text-sm"
+        >
           Porfolio
         </a>
-        <a href="/about" class="text-gray-500 hover:text-red-700">
+        <a
+          href="/about"
+          class="text-gray-500 hover:text-red-700 uppercase text-sm"
+        >
           About
         </a>
-        <a href="/contact" class="text-gray-500 hover:text-red-700">
+        <a
+          href="/contact"
+          class="text-gray-500 hover:text-red-700 uppercase text-sm"
+        >
           Contact
         </a>
       </nav>
